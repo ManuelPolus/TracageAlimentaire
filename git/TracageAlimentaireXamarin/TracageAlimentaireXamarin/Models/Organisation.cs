b0f26549672155d81316760ec93d52be75ec1b0e
Manuel Polus <manu.polus@hotmail.com>
2018-03-27 17:05:04 +0200

    ntm

---
+++ git/TracageAlimentaireXamarin/TracageAlimentaireXamarin/Models/Organisation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    public class Organisation
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telephone { get; set; }
+
+        public Adresse Adresse { get; set; }
+
+        public List<SousTraitant> SousTraitants { get; set; }
+
+
+    }
+}
---
+++ git/TracageAlimentaireXamarin/TracageAlimentaireXamarin/Models/Processus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    public class Processus
+    {
+
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public List<Etape> Etapes { get; set; }
+
+        public string Description { get; set; }
+    }
+}
